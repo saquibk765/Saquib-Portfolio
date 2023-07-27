ee1425783152a31ab8125ddc5ace97abd507b913
saquibk765 <saquibk765@gmail.com>
2023-07-28 03:14:13 +0530

    link added

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch, HashRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
-import Splash from "../pages/splash/Splash";
+// import Splash from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
 import Experience from "../pages/experience/Experience";
 import Opensource from "../pages/opensource/Opensource";
